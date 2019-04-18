5b2e7c311d251fc781b7969a07a556ccd005ae46
niyueyeee <wwy2387090@gmail.com>
2019-04-18 23:11:07 +0800

    学习代码

--- Day01_Object/src/DateDemo.java
+++ Day01_Object/src/DateDemo.java
@@ -6,10 +6,10 @@ import java.util.Date;
  * @create 2019-01-06 下午 09:46
  */
 public class DateDemo {
-    //    public static void main(String[] args) {
+//        public static void main(String[] args) {
 //        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:dd");
 //        String format = sdf.format(new Date());
-//        System.out.println(format);
+//        System.out.println(format);;
 //    }
     public static void main(String[] args) {
         SimpleDateFormat sim = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
